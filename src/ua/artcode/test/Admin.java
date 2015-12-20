05450de709166cfd863f140307f44df11516b91f
Bohdan00735 <bohdan00735@ukr.net>
2015-12-20 12:58:50 +0200

    Class admin

--- src/ua/artcode/test/Admin.java
+++ src/ua/artcode/test/Admin.java
@@ -4,11 +4,19 @@ package ua.artcode.test;
 public class Admin {
     private String login;
     private String pass;
-    private final long ID;
+    private long ID = 0;
 
-    public Admin(String login, String pass, long ID) {
+    public Admin(String login, String pass) {
         this.login = login;
         this.pass = pass;
-        this.ID = ID;
+        this.ID++;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public long getID() {
+        return ID;
     }
 }
