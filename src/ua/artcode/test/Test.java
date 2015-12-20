718e3ebc9881e4a69de0ccf43c7fbea37128b8d9
Ivpion <Ivanpiontkevich@gmail.com>
2015-12-20 14:07:19 +0200

    tests

--- src/ua/artcode/test/Test.java
+++
@@ -1,7 +0,0 @@
-package ua.artcode.test;
-
-/**
- * Created by serhii on 20.12.15.
- */
-public class Test {
-}
---
+++ src/ua/artcode/test/TestITaxiController.java
@@ -0,0 +1,59 @@
+package ua.artcode.test;
+
+import org.junit.Test;
+import ua.artcode.controller.ITaxiController;
+import ua.artcode.model.Client;
+import ua.artcode.model.Driver;
+import ua.artcode.model.Ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+/**
+ * Created by serhii on 20.12.15.
+ */
+public class TestITaxiController {
+
+ @Test
+ public void testLogin() throws Exception{
+  Client client = new Client("Login", 333333, "Home", 300, "12345");
+  assertTrue(true);
+
+ }
+
+ @Test
+ public void testGetAllClients(ITaxiController iTaxiController) throws Exception {
+  Client client1 = new Client("Login1", 333333, "Home", 300, "12345");
+  Client client2 = new Client("Login2", 333332, "Home", 300, "12345");
+
+  List<Client> clients = new ArrayList<>();
+  clients.add(client1);
+  clients.add(client2);
+  List<Client> clientsFromController = iTaxiController.getAllClients();
+  assertEquals(clients, clientsFromController);
+ }
+
+ @Test
+ public void testGetAllDrivers(ITaxiController iTaxiController) throws Exception {
+  List<Driver> driversFromController = iTaxiController.getAllDrivers();
+  List<Driver> drivers = new ArrayList<>();
+  assertEquals(drivers , driversFromController);
+ }
+
+
+ @Test
+ public void testGetTickets(ITaxiController iTaxiController) throws Exception {
+  List<Ticket> ticketsFromController = iTaxiController.getTickets();
+  List<Ticket> tickets = new ArrayList<>();
+  tickets.containsAll(ticketsFromController);
+  assertEquals(tickets, ticketsFromController);
+ }
+
+ @Test
+ public void testGetClientById(ITaxiController iTaxiController) throws Exception {
+  List<Ticket> ticketsFromController = iTaxiController.getTickets();
+
+  ticketsFromController.get()
+ }
+}
