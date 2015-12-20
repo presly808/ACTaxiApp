c0a45036195b0337187ac3ee90bff3315a1d2835
Bohdan00735 <bohdan00735@ukr.net>
2015-12-20 16:57:56 +0200

    Tickets Frame

--- src/ua/artcode/view/MenuFrame.java
+++ src/ua/artcode/view/MenuFrame.java
@@ -8,53 +8,49 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class MenuFrame extends JFrame {
-    public static void main(String[] args) {
-        new MenuFrame(null);
-    }
+    private  ITaxiController menuController;
+    private JPanel panel;
+    private JButton showTicketsButton;
+    private JButton showDriversButton;
+    private JLabel label;
 
-    private ITaxiController iTaxiController;
 
-    public MenuFrame(ITaxiController menuController) throws HeadlessException {
-        this.iTaxiController = menuController;
-        setTitle("Choosing some acction");
-        setSize(400 , 400);
+    public MenuFrame(ITaxiController menuController) {
+        this.menuController = menuController;
+        init();
+        setTitle("Taxi App");
+        setSize(200, 100);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        JPanel menuPanel = new JPanel();
-        addButtons(menuPanel);
-
+        pack();
     }
 
-    private void addButtons(JPanel menuPanel){
+    private void init() {
 
-        JButton buttonShowTikets = new JButton("Show all Tikets");
-        AcctionListenerForButtonShowTikets acctionListenerForButtonShowTikets = new AcctionListenerForButtonShowTikets();
-        buttonShowTikets.addActionListener(acctionListenerForButtonShowTikets);
-        menuPanel.add(buttonShowTikets);
+        panel = new JPanel();
+        label = new JLabel("ADMIN PANEL");
+        showTicketsButton = new JButton("Show tickets");
+        showTicketsButton.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new TicketsFrame(menuController);
+                MenuFrame.this.dispose();
+            }
+        });
 
-        JButton buttonShowDrivers = new JButton("Show all Drivers");
-        //TODO delete unnecessary variable
-        AcctionListenerForButtonShowDrivers acctionListenerForButtonShowDrivers = new AcctionListenerForButtonShowDrivers();
-        buttonShowDrivers.addActionListener(acctionListenerForButtonShowDrivers);
-        menuPanel.add(buttonShowDrivers);
-    }
+        showDriversButton = new JButton("Show drivers");
+        showDriversButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new DriversFrame();
+                MenuFrame.this.dispose();
+            }
+        });
 
-    public class AcctionListenerForButtonShowTikets implements ActionListener{
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //method who show next window with tikets
-        }
-    }
-
-    public class AcctionListenerForButtonShowDrivers implements ActionListener{
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //method who show next window with drivers
-        }
+        panel.add(showTicketsButton);
+        panel.add(showDriversButton);
+        getContentPane().add(panel);
     }
 }
