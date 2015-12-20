efa97e842d6283cec579333e935dc7e232788652
Ivpion <Ivanpiontkevich@gmail.com>
2015-12-20 17:05:07 +0200

    add tests

--- src/ua/artcode/test/TestITaxiController.java
+++ src/ua/artcode/test/TestITaxiController.java
@@ -1,6 +1,8 @@
 package ua.artcode.test;
 
+import org.junit.Before;
 import org.junit.Test;
+import ua.artcode.controller.AdminContollerFactory;
 import ua.artcode.controller.ITaxiController;
 import ua.artcode.model.Car;
 import ua.artcode.model.Client;
@@ -10,11 +12,27 @@ import ua.artcode.model.Ticket;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TestITaxiController {
 
  private ITaxiController controller; // todo assign implementation when it will be finished
+
+
+
+ @Before
+ public void setUp() throws Exception {
+  controller = AdminContollerFactory.getAdminController();
+  controller.getAllClients().add(new Client("Login1", 111111, "Home1", 100, "12345", 1));
+  controller.getAllClients().add(new Client("Login2", 222222, "Home2", 200, "12345", 2));
+  controller.getAllClients().add(new Client("Login3", 333333, "Home3", 300, "12345", 3));
+  controller.getAllDrivers().add(new Driver("Driver1", new Car(), 1));
+  controller.getAllDrivers().add(new Driver("Driver2", new Car(), 2));
+  controller.getAllDrivers().add(new Driver("Driver3", new Car(), 3));
+
+
+ }
 
  @Test
  public void testLogin() throws Exception{
@@ -46,27 +64,35 @@ public class TestITaxiController {
  public void testGetTickets() throws Exception {
   List<Ticket> ticketsFromController = controller.getTickets();
   List<Ticket> tickets = new ArrayList<>();
-  tickets.containsAll(ticketsFromController);
   assertEquals(tickets, ticketsFromController);
  }
 
  @Test
  public void testGetClientById() throws Exception {
-  List<Ticket> ticketsFromController = controller.getTickets();
-  Client client = new Client("Login", 333333, "Home", 300, "12345", 45);
-  assertEquals(client, ticketsFromController.get(1));
+  assertNotNull(controller.getClientById(controller.getAllClients().get(1).getiD()));
+  assertEquals(controller.getClientById(controller.getAllClients().get(1).getiD()).getiD(),1);
  }
 
  @Test
  public void testGetDriverBtId() throws Exception {
-  List<Driver> driversFromController = controller.getAllDrivers();
-  Driver driver = new Driver("Driver1", new Car(), 1);
-  assertEquals(driver, driversFromController.get(1));
+  assertNotNull(controller.getDriverById(controller.getAllDrivers().get(1).getiDDriver()));
+  assertEquals(controller.getDriverById(controller.getAllDrivers().get(1).getiDDriver()).getiDDriver(), 1);
+ }
+
+ @Test
+ public void testGetTicketById() throws Exception {
+ assertNotNull(controller.getTicketById(controller.getTickets().get(1).getiDTicket()));
 
  }
 
  @Test
- public void testName() throws Exception {
+ public void testSetDriverToTicket() throws Exception {
+  
+
+ }
+
+ @Test
+ public void testFindTicketByClientId() throws Exception {
 
 
  }
