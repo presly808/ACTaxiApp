8131ff334370955b0c925a376da13f2109697fbc
Bohdan00735 <bohdan00735@ukr.net>
2015-12-20 16:58:59 +0200

    tickets frame

--- src/ua/artcode/view/TicketsFrame.java
+++ src/ua/artcode/view/TicketsFrame.java
@@ -1,7 +1,34 @@
 package ua.artcode.view;
 
-/**
- * Created by sensej on 20.12.15.
- */
-public class TicketsFrame {
+
+import ua.artcode.controller.ITaxiController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TicketsFrame extends JFrame {
+    private ITaxiController taxiController;
+    private JPanel panel;
+    private JLabel label;
+    private JTable table;
+
+    public static void main(String[] args) {
+        TicketsFrame ticketsFrame = new TicketsFrame(null);
+    }
+
+    public TicketsFrame(ITaxiController taxiController) throws HeadlessException {
+        this.taxiController = taxiController;
+        init();
+        setTitle("Taxi App");
+        setSize(600 , 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void init(){
+        panel = new JPanel();
+        table = new JTable();
+
+
+    }
 }
