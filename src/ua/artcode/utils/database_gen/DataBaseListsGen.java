720ff4a4befdd2eb417ab57ff7722cce7d123d6a
Bogdan <b.handozhynski@gmail.com>
2015-12-22 00:56:20 +0200

    + replaced nulls  for tickets generation so that Ticket table would work

--- src/ua/artcode/utils/database_gen/DataBaseListsGen.java
+++ src/ua/artcode/utils/database_gen/DataBaseListsGen.java
@@ -3,6 +3,7 @@ package ua.artcode.utils.database_gen;
 import ua.artcode.model.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -10,11 +11,11 @@ import java.util.List;
  */
 public class DataBaseListsGen {
 
-    public static List<Client> clientsListGen(int size){
+    public static List<Client> clientsListGen(int size) {
 
         ArrayList<Client> clients = new ArrayList<>();
 
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
 
             clients.add(new Client("" + i, i, "" + i, "" + i, i));
 
@@ -23,40 +24,40 @@ public class DataBaseListsGen {
         return clients;
     }
 
-    public static List<Admin> adminsListGen(int size){
+    public static List<Admin> adminsListGen(int size) {
 
         ArrayList<Admin> admins = new ArrayList<>();
 
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
 
-            admins.add(new Admin("" + i, ""+i, i));
+            admins.add(new Admin("" + i, "" + i, i));
 
         }
 
         return admins;
     }
 
-    public static List<Driver> driversListGen(int size){
+    public static List<Driver> driversListGen(int size) {
 
         ArrayList<Driver> drivers = new ArrayList<>();
 
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
 
-            drivers.add(new Driver("" + i, new Car(""+i, i, ""+i), i));
+            drivers.add(new Driver("" + i, new Car("" + i, i, "" + i), i));
 
         }
 
         return drivers;
     }
 
-    public static List<Ticket> ticketsListGen(int size){
+    public static List<Ticket> ticketsListGen(int size) {
 
         ArrayList<Ticket> tickets = new ArrayList<>();
 
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
 
-            tickets.add(new Ticket(i, i, ""+i, ""+i,
-                    i, null, null, null, null, i));
+            tickets.add(new Ticket(i, i, "Pecherskiy" + i, "Dniprovskiy" + i,
+                    i, TicketStatus.NEW, new Date(), new Date(), new Date(), i));
 
         }
 
