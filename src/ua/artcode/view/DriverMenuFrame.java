d322f3f16f857778fd63235513479273f5540fc7
Bogdan <b.handozhynski@gmail.com>
2016-01-08 22:00:11 +0200

    + add "/" when reject a ticket

--- src/ua/artcode/view/DriverMenuFrame.java
+++ src/ua/artcode/view/DriverMenuFrame.java
@@ -1,5 +1,6 @@
 package ua.artcode.view;
 
+import org.jdesktop.xswingx.PromptSupport;
 import ua.artcode.controller.*;
 import ua.artcode.exception.BusyDriverException;
 
@@ -14,15 +15,15 @@ import java.awt.event.ActionListener;
 public class DriverMenuFrame extends JFrame {
 
     private DriverController controller;
-    private JPanel panel;
-    private JButton showTicketsButton, changeStatus;
+    private JPanel southPanel;
+    private JButton showTicketsButton, changeStatus, doneWithTicketButton, setLocationButton;
     private JLabel label;
+    private JPanel northPanel;
 
     public DriverMenuFrame(DriverController menuController) {
         controller = menuController;
         init();
         setTitle("Taxi App");
-        setSize(200, 100);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
@@ -32,12 +33,24 @@ public class DriverMenuFrame extends JFrame {
 
     private void init() {
 
-        panel = new JPanel();
+        southPanel = new JPanel(new GridLayout(1, 4));
+        southPanel.setBorder(BorderFactory.createEmptyBorder(5,10,10,10));
+
+        northPanel = new JPanel(new GridLayout(1, 2));
+        northPanel.setBorder(BorderFactory.createEmptyBorder(10,10,0,10));
+
+        JTextField locationField = new JTextField();
+        PromptSupport.setPrompt("Your location", locationField);
+
+
 
         //buttons
 
         showTicketsButton = new JButton("Show tickets");
-        changeStatus = new JButton("Change Status");
+        doneWithTicketButton = new JButton("Done with order");
+        changeStatus = new JButton(controller.isFree() == true ? "Start working" : "Take a day off");
+        setLocationButton = new JButton("Set location");
+
 
         // listeners
 
@@ -50,7 +63,7 @@ public class DriverMenuFrame extends JFrame {
                     if (controller.isFree() == true) {
                         changeStatus.setText("Start working");
                         changeStatus.setBackground(Color.YELLOW);
-                    }else {
+                    } else {
                         changeStatus.setText("Take a day off");
                         changeStatus.setBackground(Color.GREEN);
                     }
@@ -64,14 +77,19 @@ public class DriverMenuFrame extends JFrame {
             }
         });
 
-        showTicketsButton.addActionListener((e) -> new TicketsListFrame(controller));
+        showTicketsButton.addActionListener(e -> new TicketsListFrame(controller));
+        doneWithTicketButton.addActionListener(e -> controller.finishTrip());
+        setLocationButton.addActionListener(e -> controller.setLocation(locationField.getText()));
 
         label = new JLabel("Signed in as driver");
-
-        panel.add(label);
-        panel.add(showTicketsButton);
-        panel.add(changeStatus);
-        getContentPane().add(panel);
+        northPanel.add(locationField);
+        northPanel.add(setLocationButton);
+        southPanel.add(label);
+        southPanel.add(showTicketsButton);
+        southPanel.add(changeStatus);
+        southPanel.add(doneWithTicketButton);
+        getContentPane().add(southPanel);
+        getContentPane().add(northPanel, new BorderLayout().NORTH);
     }
 
 }
